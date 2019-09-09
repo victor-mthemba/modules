edc566dad3574bbbbd19a3ee480eb8721fde8586
victor-mthemba <victormthemba@gmail.com>
2019-09-09 12:57:58 +0200

    initial commit

---
+++ greet_xola.js
@@ -0,0 +1,18 @@
+const chalk = require('chalk');
+//import the greet module that is in the current folder
+const greet = require('./greet');
+
+const styledMessage = chalk.bgGreen.black(greet('Xola'));
+console.log(styledMessage)
+
+
+var figlet = require('figlet');
+ 
+figlet('Hello World!!', function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data)
+});
